d0b1388ccbfc9e6e57883a62472ad5513146b0a0
zJUNAIDz <64751110+zJUNAIDz@users.noreply.github.com>
2021-02-28 12:13:09 +0530

    Add files via upload

---
+++ glassmorphism/Digital Clock/main.js
@@ -0,0 +1,29 @@
+function clock() {
+  let hours = document.getElementById('hours');
+  let minutes = document.getElementById('minutes');
+  let seconds = document.getElementById('seconds');
+  let ampm = document.getElementById('ampm');
+  let hrs = new Date().getHours();
+  let mins = new Date().getMinutes();
+  let secs = new Date().getSeconds();
+  hours.innerHTML = hrs;
+  minutes.innerHTML = mins;
+  seconds.innerHTML = secs;
+  if (hrs == 0) {
+    hours.innerHTML = 1 + hrs;
+  };
+  if (secs < 10) {
+    seconds.innerHTML = '0' + secs;
+  };
+  if (mins < 10) {
+    minutes.innerHTML = '0' + mins;
+  };
+  if (hrs < 10) {
+    hours.innerHTML = '0' + hrs;
+  };
+  if (hrs > 12) {
+    hours.innerHTML = hrs - 12;
+    ampm.innerHTML = 'PM';
+  };
+};
+setInterval(clock(), 1000);
